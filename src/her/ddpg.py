dfdd1c2146ebd90ee0b46ca072f8001ef285ba9f
alankar <alankarj@andrew.cmu.edu>
2018-04-29 20:11:04 -0400

    removed incomplete changes

--- src/her/ddpg.py
+++ src/her/ddpg.py
@@ -116,14 +116,13 @@ class DDPG(object):
         }
         ret = self.sess.run(vals, feed_dict=feed)
         return ret[0]
-
     
 
-    def get_goals(self, ag, u, ag_2, e, mask):
-        ag =
-
-
-        return None
+    # def get_goals(self, ag, u, ag_2, e, mask):
+    #     ag =
+    #
+    #
+    #     return None
 
     def get_actions(self, o, ag, g, noise_eps=0., random_eps=0., use_target_net=False,
                     compute_Q=False):
