c164a62cccb43d4577367af358118383a7b5f777
Nitish Kulkarni <ni4lgi@gmail.com>
2018-04-30 03:09:48 -0400

    Add constants

---
+++ src/her/constants.py
@@ -0,0 +1,9 @@
+"""Constants for HER strategies
+"""
+
+REPLAY_STRATEGY_NONE = 'none'
+REPLAY_STRATEGY_RANDOM = 'random'
+REPLAY_STRATEGY_FUTURE = 'future'
+REPLAY_STRATEGY_LAST = 'last'
+REPLAY_STRATEGY_BEST_K = 'best_k'
+REPLAY_STRATEGY_GEN_K = 'generated_k'
